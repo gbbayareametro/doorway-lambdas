bdeb160ecf610b268aacbadbdff2396182c5e63e
Glenn Brown <gbrown@bayareametro.gov>
2024-06-11 13:05:50 -0700

    adding an await

--- lib/handler-functions/listings.ts
+++ lib/handler-functions/listings.ts
@@ -1,25 +1,37 @@
 import Fuse from "fuse.js";
-import { Listing, ListingsApi } from "../../swagger/api";
+import {
+  Listing,
+  ListingFilterParams,
+  ListingsApi,
+  ListingsStatusEnum,
+} from "../../swagger/api";
 import { MatchingError } from "./matching-error";
+import { instance } from "./winston.logger";
 
 export class ListingsInterface {
   constructor(url: string) {
     this.url = url;
+    this.logger = instance;
   }
   url: string;
+  logger: typeof instance;
   getUrl(): string {
     return this.url;
   }
   async getAllListings(): Promise<Listing[]> {
     const listingsApi = new ListingsApi(this.url);
+
     let listings: string[] = [];
-    return (await listingsApi.list()).body.items;
+    const filter = new ListingFilterParams();
+    filter.status = ListingsStatusEnum.Active;
+    return (await listingsApi.list(1, 10000, [filter])).body.items;
   }
   async listingMatcher(
     match: string,
     threshold: number = 0.25
   ): Promise<Listing> {
     const listings = await this.getAllListings();
+    await this.logger.debug(`${listings.length} total listings found`);
 
     const fuse = new Fuse(listings, {
       keys: ["name", "listingsBuildingAddress.street"],
